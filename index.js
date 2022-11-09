719343dffb7059ca01ef9ebf066de21b574bfed0
kopihannan <kopihannan3@gmail.com>
2022-11-09 23:00:38 +0600

    hide user name and password dotenv

--- index.js
+++ index.js
@@ -2,14 +2,31 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-app.use (cors());
-app.use (express.json());
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Vromon Server is running")
 })
 
-app.listen(port, ()=>{
+
+//pass: 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.risshmy.mongodb.net/?retryWrites=true&w=majority`; 
+console.log(uri); 
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.listen(port, () => {
     `Vromon server is runnig ${port}`
 })
