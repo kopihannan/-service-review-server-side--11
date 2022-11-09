e3008b4ca704b3349e4f6b3a1f133d15800744ba
kopihannan <kopihannan3@gmail.com>
2022-11-10 01:22:23 +0600

    insert data services

--- index.js
+++ index.js
@@ -13,19 +13,26 @@ app.get('/', (req, res) => {
 })
 
 
-//pass: 
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.risshmy.mongodb.net/?retryWrites=true&w=majority`; 
-console.log(uri); 
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.risshmy.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run(){
+    try{
+        const serviceCollection = client.db('vromon').collection('service');
+
+        app.get('/service', async (req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service) 
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err =>console.error(err));
 
 app.listen(port, () => {
     `Vromon server is runnig ${port}`
