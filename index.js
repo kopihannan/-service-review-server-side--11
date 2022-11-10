ff60cdbd7c0cb348e84e34c74d417411a3456833
kopihannan <kopihannan3@gmail.com>
2022-11-10 23:30:05 +0600

    vercel server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +11,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Vromon Server is running")
 })
+
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.risshmy.mongodb.net/?retryWrites=true&w=majority`;
@@ -22,10 +24,20 @@ async function run(){
         const serviceCollection = client.db('vromon').collection('service');
 
         app.get('/service', async (req, res)=>{
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
             const query = {};
             const cursor = serviceCollection.find(query);
-            const service = await cursor.toArray();
-            res.send(service) 
+            const service = await cursor.skip(page*size).limit(size).toArray();
+            const count = await serviceCollection.estimatedDocumentCount();
+            res.send({service, count}) 
+        });
+
+        app.get('/service/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
         })
     }
     finally{
