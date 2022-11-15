132c71e38cb9e1517e7f7a9da58e037a8a8ac0c1
kopihannan <kopihannan3@gmail.com>
2022-11-15 21:45:39 +0600

    review data post and get

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
@@ -20,15 +20,15 @@ const uri = "mongodb+srv://foods:4QXgCUS5PQYU2fu8@cluster0.risshmy.mongodb.net/?
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
+async function run() {
     try {
         await client.connect();
         console.log("mongoDB connected by express js kopi");
 
 
-    } 
+    }
     catch (error) {
-        
+
     }
 }
 
@@ -36,15 +36,16 @@ run().catch(console.dir);
 
 
 const Foods = client.db('service').collection('foods');
+const Review = client.db('review').collection('user');
 
-app.get('/foods', async(req, res)=>{
+app.get('/foods', async (req, res) => {
     try {
 
         const size = parseInt(req.query.limit)
- 
+
         const cursor = Foods.find().limit(size);
         const foods = await cursor.toArray();
-        res.send(foods)
+        res.send(foods);
     } catch (error) {
         console.log(error);
         res.send({
@@ -53,3 +54,55 @@ app.get('/foods', async(req, res)=>{
         })
     }
 })
+
+app.get('/foods/:id', async (req, res)=>{
+    try {
+        const id = req.params.id;
+        const query = {_id:ObjectId(id)}
+        const food = await Foods.findOne(query)
+        res.send(food)
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
+
+app.post("/user", async (req, res) => {
+    try {
+      const result = await Review.insertOne(req.body);
+  
+      if (result.insertedId) {
+        res.send({
+          success: true,
+          message: `Successfully created the ${req.body.name} with id ${result.insertedId}`,
+        });
+      } else {
+        res.send({
+          success: false,
+          error: "Couldn't create the user",
+        });
+      }
+    } catch (error) {
+      console.log(error.name.bgRed, error.message.bold);
+      res.send({
+        success: false,
+        error: error.message,
+      });
+    }
+  });
+
+  app.get("/user", async (req, res) => {
+    try {
+      const cursor = Review.find({});
+      const user = await cursor.toArray();
+  
+      res.send(user);
+    } catch (error) {
+      console.log(error.name.bgRed, error.message.bold);
+      res.send({
+        success: false,
+        error: error.message,
+      });
+    }
+  });
+  
